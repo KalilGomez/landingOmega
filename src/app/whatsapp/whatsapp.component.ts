c15543fd2684babce93a4a26c259a01ef9440651
Kalil Gomez <Kalil_Gomez@hotmail.com>
2025-08-19 23:38:54 -0300

    actualizo numero de wsp

--- src/app/whatsapp/whatsapp.component.ts
+++ src/app/whatsapp/whatsapp.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './whatsapp.component.css'
 })
 export class WhatsappComponent {
-  whatsappUrl: string = 'https://wa.me/1234567890?text=¡Hola!%20Me%20interesa%20más%20información.'; // Reemplaza con tu número y mensaje
+  whatsappUrl: string = 'https://wa.me/+5491122889080?text=¡Hola!%20Me%20interesa%20más%20información.'; // Reemplaza con tu número y mensaje
 }
