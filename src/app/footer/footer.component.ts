2de88d393e34394a421c89f5e6acacc56ddb132d
Kalil Gomez <Kalil_Gomez@hotmail.com>
2025-08-24 19:00:33 -0300

    cambio de favicon, links en footers y deslizamiento

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
@@ -49,4 +50,12 @@ export class FooterComponent {
     { name: 'Anteojos de Sol', url: '#' },
     { name: 'Reparaciones', url: '#' }
   ];
+
+  // footer.component.ts
+scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
 }
