b9a5f6cc9e896ac94c22a78741c87869d82958a5
Kalil Gomez <Kalil_Gomez@hotmail.com>
2025-09-09 20:56:36 -0300

    reestructuracion de la pagina para mejor control de los estilos

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { BannerComponent } from "../../banner/banner.component";
 
 @Component({
   selector: 'app-home',
